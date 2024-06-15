016d554fc36475bdab0345460c8119054ff3fdab
shelbymode <pocketbook.love24@gmail.com>
2024-06-15 14:38:43 +0300

    feat: update `booking sql schema`

--- apps/booking-app/src/infra/database/migrations/20240519101815_init.cjs
+++ apps/booking-app/src/infra/database/migrations/20240519101815_init.cjs
@@ -6,16 +6,26 @@ exports.up = async function (knex) {
   await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('ReserveBookingSaga', (table) => {
       table.uuid('id', { primaryKey: true }).defaultTo(knex.raw("uuid_generate_v4()"))
+      table.uuid('bookingId').nullable()
       table.jsonb('state').notNullable()
-  })
+      /**
+       * completed_step: TSagaStateUnion
+       * is_compensating_direction: boolean
+       * is_error_saga: boolean
+       */
+    })
 
   await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"').
     createTable('Booking', (table) => {
       table.uuid('id', { primaryKey: true }).defaultTo(knex.raw("uuid_generate_v4()"))
       table.integer('customer_id').notNullable()
       table.integer('course_id').notNullable()
+      table.enu('current_state', [
+        'PAYMENT_PENDING', 'APPROVAL_PENDING', 'CANCEL_PENDING',
+        'CONFIRMED', 'REJECTED', 'ERROR'
+      ])
       table.string('email').notNullable()
-  })
+    })
 };
 
 /**
